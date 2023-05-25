0b9f1ffc3ee6237c62ae13d33e25e817af4b8fab
Taqi Raza <mttalpur@gmail.com>
2023-05-25 17:40:17 +0500

    build: use halium-12 branch of halium generic adaptation build tools

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
 
-[ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b halium-11 build
+[ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b halium-12 build
 ./build/build.sh "$@"
